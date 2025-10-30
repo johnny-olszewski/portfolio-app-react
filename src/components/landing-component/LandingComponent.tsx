ab8829a58b27f6bb6901154b4ad5dee220f268ec
johnny <johnnyo.dev@gmail.com>
2025-10-30 16:36:24 -0700

    [PO-1][10/n] add signature

--- src/components/landing-component/LandingComponent.tsx
+++ src/components/landing-component/LandingComponent.tsx
@@ -11,14 +11,9 @@ import Image from "next/image";
 const LandingComponent = () => {
   const _renderHeading = () => {
     return (
-      <div className="flex flex-col items-start justify-center gap-4">
+      <div className="relative flex flex-col items-start justify-center gap-4">
         <p className="text-2xl">Hi there, I&apos;m</p>
-        <h1
-          className="text-7xl font-bold text-lime-800 bg-black/20 p-8 rounded-lg"
-          font-roboto
-        >
-          Johnny Olszewski
-        </h1>
+        <h1 className="text-7xl font-bold text-lime-800">Johnny Olszewski</h1>
         <div className="flex flex-row text-2xl gap-2">
           <h2 className="">Software Engineer.</h2>
           <h2 className="">Builder.</h2>
@@ -29,25 +24,27 @@ const LandingComponent = () => {
 
   const _renderDetails = () => {
     return (
-      <div className="flex flex-col items-start justify-center gap-6">
+      <div className="flex flex-col items-start justify-center gap-6 w-full">
         <p className="text-2xl text-gray-500">
           Whether it&apos;s building software professionally or tinkering on my
           own, I love to see my vision come to life and build someting from
           nothing.
         </p>
-        <div className="flex flex-row items-center gap-1">
-          <TbMapPin className="text-gray-400" />
-          <p>San Diego, CA</p>
-        </div>
-        <div className="flex flex-row gap-4">
-          <div className="p-2 rounded-full border border-gray-400 hover:border-gray-600 transition-colors cursor-pointer">
-            <TbBrandGithub className="text-gray-600 text-xl" />
+        <div className="flex flex-row items-center gap-2 w-full justify-between">
+          <div className="flex flex-row items-center">
+            <TbMapPin className="text-gray-400" />
+            <p>San Diego, CA</p>
           </div>
-          <div className="p-2 rounded-full border border-gray-400 hover:border-gray-600 transition-colors cursor-pointer">
-            <TbBrandLinkedin className="text-gray-600 text-xl" />
-          </div>
-          <div className="p-2 rounded-full border border-gray-400 hover:border-gray-600 transition-colors cursor-pointer">
-            <TbMail className="text-gray-600 text-xl" />
+          <div className="flex flex-row gap-4">
+            <div className="p-2 rounded-full border border-gray-400 hover:border-gray-600 transition-colors cursor-pointer">
+              <TbBrandGithub className="text-gray-600 text-xl " />
+            </div>
+            <div className="p-2 rounded-full border border-gray-400 hover:border-gray-600 transition-colors cursor-pointer">
+              <TbBrandLinkedin className="text-gray-600 text-xl" />
+            </div>
+            <div className="p-2 rounded-full border border-gray-400 hover:border-gray-600 transition-colors cursor-pointer">
+              <TbMail className="text-gray-600 text-xl" />
+            </div>
           </div>
         </div>
       </div>
@@ -55,20 +52,33 @@ const LandingComponent = () => {
   };
 
   return (
-    <div className="w-full h-full min-h-[90vh] flex flex-row items-center justify-center px-8 gap-12">
-      <div className="max-w-2xl flex flex-col items-start justify-center gap-4">
-        {_renderHeading()}
-        {_renderDetails()}
+    <div className="w-full h-full min-h-[100vh] flex flex-row items-center justify-center px-8 gap-12">
+      <div className="relative max-w-2xl">
+        <div className="absolute inset-0 -translate-x-20 -translate-y-24 bg-[#dce2d5]/40 rounded-full aspect-square h-[90%]"></div>
+        <div className="relative flex flex-col items-start justify-center gap-2 p-8">
+          {_renderHeading()}
+          {_renderDetails()}
+        </div>
       </div>
       <div className="flex flex-shrink-0">
-        <Image
-          src="/images/johnny-fine-line.png"
-          alt="Johnny Olszewski"
-          width={500}
-          height={500}
-          className="w-[50vh] h-auto"
-          priority
-        />
+        <div className="relative">
+          <Image
+            src="/images/johnny-fine-line.png"
+            alt="Johnny Olszewski"
+            width={500}
+            height={500}
+            className="w-[50vh] h-auto z-[10]"
+            priority
+          />
+
+          <Image
+            src="/images/signature.svg"
+            alt="Johnny Olszewski"
+            width={600}
+            height={200}
+            className="absolute bottom-0 translate-y-1/3 right-0 w-[400px] z-[100] h-auto pointer-events-none"
+          />
+        </div>
       </div>
     </div>
   );
